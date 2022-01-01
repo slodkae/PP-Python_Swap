0065491ca46e056391ecc52483e4b7dd628d43d9
slodkae <elena.zapolny@epitech.eu>
2022-01-01 15:04:43 +0100

    projet final

---
+++ swap.py
@@ -0,0 +1,15 @@
+i = 1
+j = 2
+k = 3
+print("la valeur de i est", i)
+print("la valeur de j est", j)
+print("la valeur de k est", k)
+
+print('')
+
+i = j
+j = k
+k = i
+print("i = j ; la valeur de i est", i)
+print("j = k ; la valeur de j est", j)
+print("k = i ;la valeur de k est", k)
